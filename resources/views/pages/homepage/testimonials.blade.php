5d957887e11822e1f60b814c32141fb61e7c02a0
jovred <jovan.redli@namics.com>
2021-05-06 15:58:01 +0200

    testimonials complete

--- resources/views/pages/homepage/testimonials.blade.php
+++ resources/views/pages/homepage/testimonials.blade.php
@@ -1,4 +1,5 @@
 <section class="testimonials-wrap">
+    <div class="bg-effect"></div>
     <div class="testimonials-content">
         <h1 class="testimonials-title">People said</h1>
         <hr class="underline center">
@@ -7,7 +8,7 @@
     <div class="testimonials-background">
         <div class="testimonials-info">
             <h1 class="testimonials-info-title">People said</h1>
-            <p class="testimonials-info-text">Litaque quam odio delenitidelenitideleniti soluta totam dolorem? Aspernatur, amet.</p>
+            <p class="testimonials-info-text">Litaque Lorem,  elit. Aliquam, doloremque. odio delenitidelenitideleniti soluta totam dolorem? Aspernatur, amet.</p>
             <a href="#" class="btn btn--xlrg btn--gradient">
                 <span class="btn__text btn-text">Reach Us</span>
             </a>
@@ -17,24 +18,32 @@
                 <div class="carousel-cell">
                     <div class="testimonials-image ai1">
                         <i class="fas fa-quote-right"></i>
-                        <p class="testimonials-info-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo cum
-                            deleniti sit corporis molestias provident eius quas amet sint ipsa!</p>
+                        <p class="testimonials-info-text">Lorem Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur excepturi alias unde nisi cupiditate asperiores. psum dolor sit amet consectetur adipisicing elit. Explicabo cum
+                            deleniti sii sit corporis molestias providenti sit corporis molestias providentt corporis molestias provident eius quas amet sint ipsa!</p>
                     </div>
                     <h2 class="testimonials-name">John Doe</h2>
                 </div>
                 <div class="carousel-cell">
                     <div class="testimonials-image ai1">
                         <i class="fas fa-quote-right"></i>
-                        <p class="testimonials-info-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo cum
-                            deleniti sit corporis molestias provident eius quas amet sint ipsa!</p>
+                        <p class="testimonials-info-text">Lorem Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur excepturi alias unde nisi cupiditate asperiores. psum dolor sit amet consectetur adipisicing elit. Explicabo cum
+                            deleniti sii sit corporis molestias providenti sit corporis molestias providentt corporis molestias provident eius quas amet sint ipsa!</p>
                     </div>
                     <h2 class="testimonials-name">John Doe</h2>
                 </div>
                 <div class="carousel-cell">
                     <div class="testimonials-image ai1">
                         <i class="fas fa-quote-right"></i>
-                        <p class="testimonials-info-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo cum
-                            deleniti sit corporis molestias provident eius quas amet sint ipsa!</p>
+                        <p class="testimonials-info-text">Lorem Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur excepturi alias unde nisi cupiditate asperiores. psum dolor sit amet consectetur adipisicing elit. Explicabo cum
+                            deleniti sii sit corporis molestias providenti sit corporis molestias providentt corporis molestias provident eius quas amet sint ipsa!</p>
+                    </div>
+                    <h2 class="testimonials-name">John Doe</h2>
+                </div>
+                <div class="carousel-cell">
+                    <div class="testimonials-image ai1">
+                        <i class="fas fa-quote-right"></i>
+                        <p class="testimonials-info-text">Lorem Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur excepturi alias unde nisi cupiditate asperiores. psum dolor sit amet consectetur adipisicing elit. Explicabo cum
+                            deleniti sii sit corporis molestias providenti sit corporis molestias providentt corporis molestias provident eius quas amet sint ipsa!</p>
                     </div>
                     <h2 class="testimonials-name">John Doe</h2>
                 </div>
